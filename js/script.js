248cba2e0b1e926c7a4131b1739f313a64004dab
hattrick <dasha.khitrick@gmail.com>
2022-12-21 00:28:11 +0300

    adaptive of all axit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$(".header__burger, .header__menu").toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+new Swiper('.customers-slider', {
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	}, 
+	
+	centeredSlides: true,
+	initialSlide: 1,
+	loop: true,
+});
